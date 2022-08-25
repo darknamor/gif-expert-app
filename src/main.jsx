2ede791aa4ba9df9488740533ccaae696b4f40e8
Nicolás Miranda <darknamor@gmail.com>
2022-08-25 11:37:43 -0400

    add styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { GifExpertApp } from './GifExpertApp';
+import { GifExpertApp } from './Components/GifExpertApp';
+import './Styles/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
