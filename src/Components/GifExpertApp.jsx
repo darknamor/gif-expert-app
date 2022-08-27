a60f05806bfbb3a54db126ea0c8b5bd410bc941e
Nicolás Miranda <darknamor@gmail.com>
2022-08-27 12:58:29 -0400

    call service

--- src/Components/GifExpertApp.jsx
+++ src/Components/GifExpertApp.jsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+import { AddCategory } from './AddCategory';
+import { GifGrid } from './GifGrid';
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['One Punch', 'Dragonball']);
+  const onAddCategory = (e) => {
+    if (categories.includes(e)) return;
+    setCategories([...categories, e]);
+  };
   return (
     <>
       <h1>GifExpertApp</h1>
+      <AddCategory onAddCategory={onAddCategory} />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
     </>
   );
 };
